7d83ac661fd548066d2c06d441e0253fd2be72c2
romarioalvesdev <romariotecla@icloud.com>
2024-11-11 17:31:18 -0300

    atualizacaoNoJS

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const botaoConverter = document.querySelector(".converter")
 
 function converterValor(){
-    const valorDigitado = document.querySelector(".valor-da-moeda").value
-    
-    
+    const valorDigitado = document.querySelector(".converter-botao").value
+    const valorDaMoedaAconverter = document.querySelector(".valor-da-moeda-a-converter")
+    const valorDaMoedaConvertida = document.querySelector(".valor-da-moeda-convertida")
+
     const valorDoDia = 5.2
     const valorConvertido = valorDigitado / valorDoDia
     console.log(valorConvertido)

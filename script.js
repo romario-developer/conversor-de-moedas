4133522ebd8ed16fae9c3eaab3cbbaf648436d64
romario-developer <romariotecla@icloud.com>
2024-11-11 23:53:51 -0300

    conversor de moedas finalizado

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-const botaoConverter = document.querySelector(".converter")
+const botaoDeConverter = document.querySelector(".botaodeconverter");
+const valorAConverter = document.querySelector(".valor-a-converter");
+const valorConvertido = document.querySelector(".valor-convertido");
+const selecaoDaMoeda = document.querySelector(".selecao-da-moeda");
 
-function converterValor(){
-    const valorDigitado = document.querySelector(".converter-botao").value
-    const valorDaMoedaAconverter = document.querySelector(".valor-da-moeda-a-converter")
-    const valorDaMoedaConvertida = document.querySelector(".valor-da-moeda-convertida")
+function converterValor() {
+  const valorDigitado = document.querySelector(".converter-botao").value;
 
-    const valorDoDia = 5.2
-    const valorConvertido = valorDigitado / valorDoDia
-    valorDaMoedaAconverter.innerHTML = valorDigitado
+  const dolarDoDia = 5.81;
+  const euroDoDia = 6.13;
 
-    console.log(valorConvertido)
+  if (selecaoDaMoeda.value == "dolar") {
+    valorConvertido.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(valorDigitado / dolarDoDia);
+  }
+
+  if (selecaoDaMoeda.value == "euro") {
+    valorConvertido.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(valorDigitado / euroDoDia);
+  }
+
+  valorAConverter.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(valorDigitado);
 }
 
-
-botaoConverter.addEventListener("click", converterValor)
+botaoDeConverter.addEventListener("click", converterValor);
